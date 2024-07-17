9e74f8350124197c847af3a1d8fbf9b67f7dabbe
rashmin <rashminkachhadiya97273@gmail.com>
2024-07-17 13:40:18 +0100

    'changes'

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -27,7 +27,7 @@
                                             <a href="tel:07930 906567" style="text-decoration: none;"><i class="fa fa-phone"></i> </a> 
                                         </div>
                                         <div class="col-auto">
-                                            <a style="text-decoration: none;" href="tel:07930 906567">Call +44(0) 7930 906567 <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 256 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg> </a>
+                                            <a style="text-decoration: none;" href="tel:07376003131">Call +44(0) 7376003131 <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 256 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg> </a>
                                         </div>
                                         
                                     </li>
